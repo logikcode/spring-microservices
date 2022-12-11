e48c064d37aab8dffb352b90a710ba0911dc242e
Emmanuel Oladipupo <emmanuel.oladipupo@decagon.dev>
2022-12-11 22:33:35 +0100

    project initialization

---
+++ src/test/java/com/manuel/springmicroservicesproject/SpringMicroservicesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manuel.springmicroservicesproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroservicesProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
