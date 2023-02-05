3838ee90e807318da937609bc5833f3c09a0468f
Emmanuel Oladipupo <emmanuel.oladipupo@decagon.dev>
2023-02-05 15:23:30 +0100

    add services fallback retries with resilience4j

---
+++ currency-conversion/src/main/java/com/manuel/currencyconversion/controllers/CircuitBreakerController.java
@@ -0,0 +1,24 @@
+package com.manuel.currencyconversion.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@Retry(name = "sample-api", fallbackMethod= "hardcodedResponse")
+public class CircuitBreakerController {
+    private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+    @GetMapping("/sample-api")
+    public String sampleApi(){
+        logger.info("sample Api call invoked");
+        ResponseEntity<String> responseEntity = new RestTemplate().getForEntity("http://localhost:8080/dummy-api", String.class);
+        return responseEntity.getBody();
+    }
+    public String hardcodedResponse (Exception exception){
+
+        return "fallback-response";
+    }
+}
